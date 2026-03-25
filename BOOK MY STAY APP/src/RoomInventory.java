df6e22f96a9d7d278370adba6bc9761b924996f4
ra5922 <ra5922@srmist.edu.in>
2026-03-25 09:25:43 +0530

    UC6 3  code finished

---
+++ BOOK MY STAY APP/src/RoomInventory.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+
+        inventory.put("Single Room", 2);
+        inventory.put("Double Room", 2);
+        inventory.put("Suite Room", 1);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int count) {
+        inventory.put(roomType, count);
+    }
+
+    public void displayInventory() {
+        System.out.println("\n=== Final Inventory ===");
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
